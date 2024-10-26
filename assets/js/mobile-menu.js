2c6f81024a919a1481f929263e56187cd5fa3c7d
vinu-ux <vineethes8@gmail.com>
2024-10-26 20:38:52 +0530

    added

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -6,12 +6,17 @@
     const mobile_nav_open = $('.mobile-nav-icon');
     const mobile_sidebar = $('.mobile-sidebar');
     const mobile_nav_close = $('.menu-close');
+    const mobile_nav_hide = $('.menu-hide');
 
     mobile_nav_open.on('click', function(){
       mobile_sidebar.addClass('mobile-menu-active');
     });
 
     mobile_nav_close.on('click', function(){
+      mobile_sidebar.removeClass('mobile-menu-active');
+    });
+
+    mobile_nav_hide.on('click', function(){
       mobile_sidebar.removeClass('mobile-menu-active');
     });
 
